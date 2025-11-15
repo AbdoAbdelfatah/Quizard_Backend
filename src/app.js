806b6dfd987b888a4a30834b9458ec6ae78ace5d
AbdoAbdelfatah <abdofatah410@gmail.com>
2025-11-15 13:48:14 +0200

    multer for photo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import express from "express";
 import { globalResponse } from "./middlewares/globalErrorHandler.middleware.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 import userRouter from "./modules/user/user.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadsDir = path.join(__dirname, "uploads", "profiles");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 const app = express();
 
 app.use(express.json());
 
 // Define your routes here
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/v1/users", userRouter);
+
 // global error handler
 app.use(globalResponse);
 
